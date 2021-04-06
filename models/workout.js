78a221343712ec705162030c727db707e2538106
vshulman25 <victoriashulman25@gmail.com>
2021-04-06 14:05:17 -0400

    had issue with original folder structure andgetmoving repo, new repo

---
+++ models/workout.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    exercises: [
+        {
+          type: {
+              type: String,
+              trim: true,
+              required: "Enter your exercise"
+          },
+          name: {
+            type: String,
+            trim: true,
+            required: "Enter your name"
+        },
+          duration: {
+            type: Number,
+            trim: true
+        },
+          weight: {
+            type: Number,
+            trim: true
+        },
+          reps: {
+            type: Number,
+            trim: true
+        },
+          sets: {
+            type: Number,
+            trim: true
+        },
+        }
+      ]
+
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
---
+++ routes/api.js
@@ -0,0 +1,40 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+  .then((dbWorkout) => {
+    res.json(dbWorkout);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
+});
+
+router.put('/api/workouts/:id', ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    // "runValidators" will ensure new exercises meet our schema requirements
+    { new: true, runValidators: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/transaction", (req, res) => {
+  Transaction.find({})
+    .sort({ date: -1 })
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
